0eb2372c67c0a054b5d29369e9391ea96ee5d4a6
Samuel Kang <samuel.june.kang@gmail.com>
2025-05-17 20:29:03 -0700

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 
+const animalUsers = ['cat', 'dog', 'fish'];
+const plantUsers = ['rose', 'tulip', 'daisy'];
 
+require('datejs')
+
+// The function should return a single array containing all the usernames
+function combineUsers(...args) {
+
+  const combinedObject = {
+    users: [],
+    merge_date: new Date().toString('M/d/yyyy')
+  }
+
+  combinedObject.users = [].concat(...args)
+  return combinedObject
+}
+
+console.log(combineUsers(animalUsers, plantUsers));
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
